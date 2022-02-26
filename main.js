753040686810b56f7c0dc6c1ae34ecdba239b37d
celebrater <tkgm45nbchk@gmail.com>
2022-02-26 23:09:06 +0900

    scroll to next page succeeded. but moving is odd

--- main.js
+++ main.js
@@ -3,12 +3,32 @@
 let prev = document.getElementById("prev");
 let current = document.getElementById("current");
 let next = document.getElementById("next");
+let pageNum = 0;
+const width = window.innerWidth;
+const height = window.innerHeight;
 // let div = document.getElementById("test");
 
 // const addLastP = () => {
 //     let p1 = document.createElement("p");
 //     let p2 = document.createElement("p");
 //     let p3 = document.createElement("p");
+// }
+
+const scrollWindow = (w, h, ms, smooth) => {
+    return new Promise((resolve, reject)=> {
+        setTimeout(()=> {
+            window.scrollTo({
+                left: w,
+                top: h,
+                behavior: smooth ? "smooth" : "auto"
+            });
+            resolve();
+        }, ms);
+    });
+}
+
+// const sayHello = async(name) => {
+//     return await asyncReturnSomething(`Hello! I'm ${name}!`)
 // }
 
 const createLines = () => {
@@ -29,6 +49,31 @@ const createLines = () => {
         // console.log(i);
     }
     // return lines;
+    window.scrollTo({
+        left: width * 1.1
+        // behavior: 'smooth'
+    });
+}
+
+const buttonClicked = async(lr) => {
+    if(lr === "left"){
+        if(pageNum > 0){
+            pageNum--;
+        }
+    } else {
+        if(pageNum < 3){
+            pageNum++;
+        }
+    }
+    await scrollWindow(width * 0.1, 0, 1, true);
+    await scrollWindow(width * 1.1, height * 0.95, 1000, false);
+    // window.scrollTo({
+    //     // left: pageNum * width + width * 0.1,
+    //     left: width * 1.1,
+    //     top: height,
+    //     behavior: 'smooth'
+    // });
+    console.log("pageNum: " + pageNum);
 }
 
 // div.innerHTML = createLines();
