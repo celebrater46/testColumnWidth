c3dd91c3354d1c9bdbdd00199dea9cbdb74eeb4b
celebrater <tkgm45nbchk@gmail.com>
2022-02-26 18:47:54 +0900

    displayed div#prev, div#current and div#next successfully

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 "use strict";
 
-let div = document.getElementById("test");
+let prev = document.getElementById("prev");
+let current = document.getElementById("current");
+let next = document.getElementById("next");
+// let div = document.getElementById("test");
+
+// const addLastP = () => {
+//     let p1 = document.createElement("p");
+//     let p2 = document.createElement("p");
+//     let p3 = document.createElement("p");
+// }
 
 const createLines = () => {
     let lines = [];
     const array = sampleTexts.join().split("\n");
     console.log(array);
     for (let i = 0; i < array.length; i++){
-        let p = document.createElement("p");
-        p.innerText = array[i];
-        div.appendChild(p);
-        console.log(i);
+        let p1 = document.createElement("p");
+        let p2 = document.createElement("p");
+        let p3 = document.createElement("p");
+        const line = array[i] === "" ? "　" : array[i];
+        p1.innerText = line;
+        p2.innerText = line;
+        p3.innerText = line;
+        prev.appendChild(p1);
+        current.appendChild(p2);
+        next.appendChild(p3);
+        // console.log(i);
     }
     // return lines;
 }
